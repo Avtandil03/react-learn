a50f5be42664591a5a5416759af96a99339b66c4
Avtandil <avtandilqazxswedc@gmail.com>
2023-01-23 20:14:29 +0300

    add search input

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import PostList from "./components/PostList";
 import "./styles/App.css"
 import PostForm from "./components/PostForm";
 import MySelect from "./UI/select/MySelect";
-import { setSelectionRange } from "@testing-library/user-event/dist/utils";
+import MyInput from "./UI/input/MyInput";
 
 function App() {  
   
@@ -13,7 +13,20 @@ function App() {
     {id: 3, title: "a", body: "b"},
   ])
 
-  const [selectedSort, SetSelectedSort] = useState('');
+  const [selectedSort, setSelectedSort] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+
+  function getSortedPosts() {
+    if(selectedSort){
+      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+    }
+
+    return posts;
+
+  }
+
+  const sortedPosts = getSortedPosts()
+
   function createPost(newPost){
     setPosts([...posts, newPost]);
   }
@@ -23,30 +36,35 @@ function App() {
   }
 
   function sortPosts(sort){
-    SetSelectedSort(sort)
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
-    console.log(sort)
+    setSelectedSort(sort)
   }
 
   
   return (
     <div className="App">
-      <PostForm create={createPost} posts={posts}/>
-      <hr style={{margin: '15px 0', borderTop: '1px solid gray'}}/>
-      <MySelect 
-        value={selectedSort}
-        onChange={sortPosts}
-        options={[
-          {value: 'title', name: 'По названию'},
-          {value: 'body', name: 'По описанию'}
-        ]} 
-        defaultValue='Сортировка'/>
-      {posts.length 
-        ? 
-          <PostList posts={posts} title="Список постов" remove={removePost} />
-        :
-          <h1 style={{textAlign: 'center'}}> Posts not found =( </h1>
-      }
+      <PostForm create={createPost} posts={posts} />
+      <hr style={{ margin: "15px 0", borderTop: "1px solid gray" }} />
+      <div>
+        <MyInput
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+          placeholder='Поиск... '
+        />
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          options={[
+            { value: "title", name: "По названию" },
+            { value: "body", name: "По описанию" },
+          ]}
+          defaultValue="Сортировка"
+        />
+      </div>
+      {posts.length ? (
+        <PostList posts={sortedPosts } title="Список постов" remove={removePost} />
+      ) : (
+        <h1 style={{ textAlign: "center" }}> Posts not found =( </h1>
+      )}
     </div>
   );
 }
